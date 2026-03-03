5267d2a924ecd56c6e9ff1fd386549025fa4c6cc
Harry7986 <emarh.harry.code@gmail.com>
2026-03-03 15:42:24 +0100

    App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import './App.css'
+import { FetchDemo } from './components/FetchDemo';
 
 function App() {
-
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+  return <FetchDemo />;
 }
 
-export default App
+export default App;
